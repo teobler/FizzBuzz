4470d6204a183d8ff536d5ef0f39efce31fee738
qirong <teobler@163.com>
2020-01-09 21:28:06 +0800

    return FizzBuzz given a number is multiples of 3 and 5.

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -2,6 +2,10 @@ package com.thoughtworks;
 
 public class FizzBuzzGame {
   public static String fizzBuzz(Integer number) {
+    if (number % 3 == 0 && number % 5 == 0) {
+      return "FizzBuzz";
+    }
+
     if (number % 3 == 0) {
       return "Fizz";
     }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -19,4 +19,9 @@ public class FizzBuzzGameTest {
   public void should_return_Whizz_given_number_is_multiples_of_7() {
     assertEquals(FizzBuzzGame.fizzBuzz(77), "Whizz");
   }
+
+  @Test
+  public void should_return_FizzBuzz_given_number_is_multiples_of_3_and_5() {
+    assertEquals(FizzBuzzGame.fizzBuzz(15), "FizzBuzz");
+  }
 }
