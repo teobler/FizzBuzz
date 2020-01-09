b733a0026acc24d16e5296db5252bb4fcd7dc404
qirong <teobler@163.com>
2020-01-09 22:42:17 +0800

    refactor - split whole task as two parts - contains 357 and multiples of 357.

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -2,19 +2,19 @@ package com.thoughtworks;
 
 public class FizzBuzzGame {
   public static String fizzBuzz(Integer number) {
+    if (isContains357(number)) {
+      return handleNumberContains357(number);
+    }
+
+    if (isMultiplesOf357(number)) {
+      return handleNumberMultiplesOf357(number);
+    }
+
+    return String.valueOf(number);
+  }
+
+  private static String handleNumberMultiplesOf357(Integer number) {
     String result = "";
-
-    if (String.valueOf(number).contains("7")) {
-      return "Fizz";
-    }
-
-    if (String.valueOf(number).contains("5")) {
-      return "BuzzWhizz";
-    }
-
-    if (String.valueOf(number).contains("3")) {
-      return "Fizz";
-    }
 
     if (number % 3 == 0) {
       result = result + "Fizz";
@@ -28,6 +28,28 @@ public class FizzBuzzGame {
       result = result + "Whizz";
     }
 
-    return "".equals(result) ? String.valueOf(number) : result;
+    return result;
+  }
+
+  private static boolean isMultiplesOf357(Integer number) {
+    return number % 3 == 0 || number % 5 == 0 || number % 7 == 0;
+  }
+
+  private static boolean isContains357(Integer number) {
+    return String.valueOf(number).contains("3")
+        || String.valueOf(number).contains("5")
+        || String.valueOf(number).contains("7");
+  }
+
+  private static String handleNumberContains357(Integer number) {
+    if (String.valueOf(number).contains("7")) {
+      return "Fizz";
+    }
+
+    if (String.valueOf(number).contains("5")) {
+      return "BuzzWhizz";
+    }
+
+    return "Fizz";
   }
 }
