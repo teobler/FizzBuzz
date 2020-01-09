2f3519113b9cf8dfb6ed9ee288c7911ab3ce219a
qirong <teobler@163.com>
2020-01-09 21:40:48 +0800

    refactor - use a string flag to collect result instead of return result once.

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -2,34 +2,20 @@ package com.thoughtworks;
 
 public class FizzBuzzGame {
   public static String fizzBuzz(Integer number) {
-    if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
-      return "FizzBuzzWhizz";
-    }
-
-    if (number % 5 == 0 && number % 7 == 0) {
-      return "BuzzWhizz";
-    }
-
-    if (number % 3 == 0 && number % 7 == 0) {
-      return "FizzWhizz";
-    }
-
-    if (number % 3 == 0 && number % 5 == 0) {
-      return "FizzBuzz";
-    }
+    String result = "";
 
     if (number % 3 == 0) {
-      return "Fizz";
+      result = result + "Fizz";
     }
 
     if (number % 5 == 0) {
-      return "Buzz";
+      result = result + "Buzz";
     }
 
     if (number % 7 == 0) {
-      return "Whizz";
+      result = result + "Whizz";
     }
 
-    return String.valueOf(number);
+    return "".equals(result) ? String.valueOf(number) : result;
   }
 }
