968b10642afc40a389ce3342c4801566ce4916ce
teobler <teobler@163.com>
2020-01-10 14:03:27 +0800

    refactor - contains 5 or 7 should be two different parts

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -3,25 +3,30 @@ package com.thoughtworks;
 public class FizzBuzzGame {
   public static String fizzBuzz(Integer number) {
     String stringNumber = String.valueOf(number);
+    String result = "";
 
-    if (stringNumber.contains("7") && number % 7 == 0 && number % 3 == 0) {
-      return "FizzWhizz";
+    if (stringNumber.contains("7")) {
+      if (number % 3 == 0) {
+        result = result + "Fizz";
+      }
+
+      if (number % 7 == 0) {
+        result = result + "Whizz";
+      }
+
+      return result;
     }
 
-    if (stringNumber.contains("7") && number % 3 == 0) {
-      return "Fizz";
-    }
+    if (stringNumber.contains("5")) {
+      if (number % 5 == 0) {
+        result = result + "Buzz";
+      }
 
-    if (stringNumber.contains("7") && number % 7 == 0) {
-      return "Whizz";
-    }
+      if (number % 7 == 0) {
+        result = result + "Whizz";
+      }
 
-    if (stringNumber.contains("5") && number % 7 == 0) {
-      return "BuzzWhizz";
-    }
-
-    if (stringNumber.contains("5") && number % 5 == 0) {
-      return "Buzz";
+      return result;
     }
 
     return stringNumber;
