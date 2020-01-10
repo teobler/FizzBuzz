1988055fd1b0c4a8018f3f3c3fd08bed2f91fd09
teobler <teobler@163.com>
2020-01-10 13:55:08 +0800

    should return FizzWhizz if a number contains 7 and is multiples of 3 and 7

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -1,21 +1,25 @@
 package com.thoughtworks;
 
 public class FizzBuzzGame {
-    public static String fizzBuzz(Integer number) {
-        String stringNumber = String.valueOf(number);
+  public static String fizzBuzz(Integer number) {
+    String stringNumber = String.valueOf(number);
 
-        if (stringNumber.contains("7") && number % 3 == 0) {
-            return "Fizz";
-        }
-
-        if (stringNumber.contains("7") && number % 7 == 0) {
-            return "Whizz";
-        }
-
-        if (stringNumber.contains("5") && number % 5 == 0) {
-            return "Buzz";
-        }
-
-        return stringNumber;
+    if (stringNumber.contains("7") && number % 7 == 0 && number % 3 == 0) {
+      return "FizzWhizz";
     }
+
+    if (stringNumber.contains("7") && number % 3 == 0) {
+      return "Fizz";
+    }
+
+    if (stringNumber.contains("7") && number % 7 == 0) {
+      return "Whizz";
+    }
+
+    if (stringNumber.contains("5") && number % 5 == 0) {
+      return "Buzz";
+    }
+
+    return stringNumber;
+  }
 }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,24 +2,29 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzGameTest {
   @Test
   public void should_return_Fizz_given_a_number_contains_7_and_is_multiples_of_3() {
-      assertEquals(FizzBuzzGame.fizzBuzz(75), "Fizz");
-      assertEquals(FizzBuzzGame.fizzBuzz(174), "Fizz");
+    assertEquals(FizzBuzzGame.fizzBuzz(75), "Fizz");
+    assertEquals(FizzBuzzGame.fizzBuzz(174), "Fizz");
   }
 
   @Test
   public void should_return_Whizz_given_a_number_contains_7_and_is_multiples_of_7() {
-      assertEquals(FizzBuzzGame.fizzBuzz(77), "Whizz");
-      assertEquals(FizzBuzzGame.fizzBuzz(7), "Whizz");
+    assertEquals(FizzBuzzGame.fizzBuzz(77), "Whizz");
+    assertEquals(FizzBuzzGame.fizzBuzz(7), "Whizz");
+  }
+
+  @Test
+  public void should_return_FizzWhizz_given_a_number_contains_7_and_is_multiples_of_3_and_7() {
+    assertEquals(FizzBuzzGame.fizzBuzz(567), "FizzWhizz");
   }
 
   @Test
   public void should_return_Buzz_given_a_number_contains_5_and_is_multiples_of_5() {
-      assertEquals(FizzBuzzGame.fizzBuzz(55), "Buzz");
-      assertEquals(FizzBuzzGame.fizzBuzz(35), "Buzz");
+    assertEquals(FizzBuzzGame.fizzBuzz(55), "Buzz");
+    assertEquals(FizzBuzzGame.fizzBuzz(35), "Buzz");
   }
 }
