078ea6e0b66c24be7b9103a7837644a79be9c959
teobler <teobler@163.com>
2020-01-10 14:25:15 +0800

    refactor - split code as "contains" part and "multiple" part

--- src/main/java/com.thoughtworks/FizzBuzzGame.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame.java
@@ -3,6 +3,19 @@ package com.thoughtworks;
 public class FizzBuzzGame {
   public static String fizzBuzz(Integer number) {
     String stringNumber = String.valueOf(number);
+
+    if (isStringNumberContains357(stringNumber)) {
+      return handleStringNumberContains357(stringNumber, number);
+    }
+
+    return handleMultipleCase(number);
+  }
+
+  private static boolean isStringNumberContains357(String stringNumber) {
+    return stringNumber.contains("3") || stringNumber.contains("5") || stringNumber.contains("7");
+  }
+
+  private static String handleStringNumberContains357(String stringNumber, Integer number) {
     String result = "";
 
     if (stringNumber.contains("7")) {
@@ -29,11 +42,13 @@ public class FizzBuzzGame {
       return result;
     }
 
-    if (stringNumber.contains("3")) {
-      result = result + "Fizz";
+    result = result + "Fizz";
 
-      return result;
-    }
+    return result;
+  }
+
+  private static String handleMultipleCase(Integer number) {
+    String result = "";
 
     if (number % 3 == 0) {
       result = result + "Fizz";
@@ -47,6 +62,6 @@ public class FizzBuzzGame {
       result = result + "Whizz";
     }
 
-    return "".equals(result) ? stringNumber : result;
+    return "".equals(result) ? String.valueOf(number) : result;
   }
 }
