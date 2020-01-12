9cf50b15228e3acaf4f44d728cff8f4dc1b91976
teobler <teobler@163.com>
2020-01-12 22:34:48 +0800

    [2] refactor - extract common logic.

--- src/main/java/com.thoughtworks/FizzBuzzGame2.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame2.java
@@ -19,53 +19,63 @@ public class FizzBuzzGame2 {
     return handleCaseDoesNotContains357(number);
   }
 
-  private static String handleCaseDoesNotContains357(Integer number) {
-    String result = "";
-
-    if (number % 3 == 0) {
-      result += "Fizz";
-    }
-
-    if (number % 5 == 0) {
-      result += "Buzz";
-    }
-
-    if (number % 7 == 0) {
-      result += "Whizz";
-    }
-
-    return "".equals(result) ? number.toString() : result;
-  }
-
-  private static String handleCaseContains5(Integer number) {
-    String result = "";
-
-    if (number % 5 == 0) {
-      result += "Buzz";
-    }
-
-    if (number % 7 == 0) {
-      result += "Whizz";
-    }
-
-    return "".equals(result) ? number.toString() : result;
-  }
-
   private static String handleCaseContains7(Integer number) {
     String result = "";
 
-    if (number % 3 == 0) {
-      result += "Fizz";
-    }
-
-    if (number % 7 == 0) {
-      result += "Whizz";
-    }
+    result = handleDivide3(number, result);
+    result = handleDivide7(number, result);
 
     if (number.toString().contains("3") && "".equals(result)) {
       result = "Fizz";
     }
 
+    return getResult(number, result);
+  }
+
+  private static String handleCaseContains5(Integer number) {
+    String result = "";
+
+    result = handleDivide5(number, result);
+    result = handleDivide7(number, result);
+
+    return getResult(number, result);
+  }
+
+  private static String handleCaseDoesNotContains357(Integer number) {
+    String result = "";
+
+    result = handleDivide3(number, result);
+    result = handleDivide5(number, result);
+    result = handleDivide7(number, result);
+
+    return getResult(number, result);
+  }
+
+  private static String handleDivide3(Integer number, String result) {
+    if (number % 3 == 0) {
+      result += "Fizz";
+    }
+
+    return result;
+  }
+
+  private static String handleDivide5(Integer number, String result) {
+    if (number % 5 == 0) {
+      result += "Buzz";
+    }
+
+    return result;
+  }
+
+  private static String handleDivide7(Integer number, String result) {
+    if (number % 7 == 0) {
+      result += "Whizz";
+    }
+
+    return result;
+  }
+
+  private static String getResult(Integer number, String result) {
     return "".equals(result) ? number.toString() : result;
   }
 }
