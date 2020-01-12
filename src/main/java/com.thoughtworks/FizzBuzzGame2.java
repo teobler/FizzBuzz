42af71d39cd3199b95802e96e5f82787e66a1819
teobler <teobler@163.com>
2020-01-12 20:56:42 +0800

    [2] should return Fizz given a number contains 37 and is not multiple of 37.

--- src/main/java/com.thoughtworks/FizzBuzzGame2.java
+++ src/main/java/com.thoughtworks/FizzBuzzGame2.java
@@ -4,6 +4,10 @@ public class FizzBuzzGame2 {
   public static String fizzBuzz(Integer number) {
     String stringNumber = number.toString();
 
+    if (stringNumber.contains("7") && stringNumber.contains("3") && number % 7 != 0 && number % 3 != 0) {
+      return "Fizz";
+    }
+
     if (stringNumber.contains("7") && number % 7 == 0 && number % 3 == 0) {
       return "FizzWhizz";
     }
--- src/test/java/com/thoughtworks/FizzBuzzGame2Test.java
+++ src/test/java/com/thoughtworks/FizzBuzzGame2Test.java
@@ -21,4 +21,9 @@ public class FizzBuzzGame2Test {
   public void should_return_FizzWhizz_given_a_number_contains_7_and_is_multiple_of_37() {
     assertEquals("FizzWhizz", FizzBuzzGame2.fizzBuzz(567));
   }
+
+  @Test
+  public void should_return_Fizz_given_a_number_contains_37_and_is_not_multiple_of_37() {
+    assertEquals("Fizz", FizzBuzzGame2.fizzBuzz(37));
+  }
 }
